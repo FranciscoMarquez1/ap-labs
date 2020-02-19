ed4721d0cf388f3aa38365cc6521b4f2000ab925
FranciscoMarquez1 <fcomarquezb@hotmail.com>
2020-02-18 20:33:23 -0600

    Solve first challenge
    Signed-off-by: FranciscoMarquez1 <fcomarquezb@hotmail.com>

---
+++ labs/first-challenge/firstchallenge.py
@@ -0,0 +1,16 @@
+def longestSubstring (string):
+    result = 0
+    for i in range(len(string)):
+        substring = set()
+        counter = 0
+        for j in range(i, len(string)):
+            if string[j] not in substring:
+                substring.add(string[j])
+                counter += 1
+            else:
+                break
+        if counter > result:
+            result = counter
+    return result
+
+
