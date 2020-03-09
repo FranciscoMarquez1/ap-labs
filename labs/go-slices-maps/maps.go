799229ab8d7e2f5243209e0653a4aa993fd41120
FranciscoMarquez1 <fcomarquezb@hotmail.com>
2020-03-09 10:33:41 -0600

    Solved go-slices-maps

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	counts := make(map[string]int)
+	for _, word := range strings.Split(string(s), " ") {
+		counts[word]++
+	}
+	return counts
+}
+
 func main() {
+	wc.Test(WordCount)
 }
