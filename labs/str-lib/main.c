a727e916082a9d927b973d4559f66cfdd85d2465
FranciscoMarquez1 <fcomarquezb@hotmail.com>
2020-03-21 22:46:39 -0600

    Solved strlib lab

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
-int main() {
+
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char *argv[]) {
+    char* originalStr = argv[1];
+    char* addStr = argv[2];
+    char* subStr = argv[3];
+    char* concStr = mystradd(originalStr, addStr);
+
+    printf("Initial Lenght     : %d\n", mystrlen(originalStr));
+    printf("New String         : %s\n", concStr);
+
+    if (mystrfind(concStr, subStr)==1){
+        printf("SubString was found: yes\n");
+    } else {
+        printf("SubString was found: no\n");
+    }
+    
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,50 @@
 
 
 int mystrlen(char *str){
-    return 0;
+    int len = 0;
+    for(int i=0;str[i]!='\0';i++){
+        len++;
+    }
+    return len;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    char* concatenate;
+    int lenOr = mystrlen(origin);
+    int lenAd = mystrlen(addition);
+    int lenConc = lenOr + lenAd;
+    concatenate = malloc(sizeof(char)*(lenConc+1));
+    int counter = 0;
+    for(int i=0;i<lenConc;i++){
+        if (i<lenOr){
+            concatenate[i]=origin[i];
+        } else {
+            concatenate[i]=addition[counter];
+            counter++;
+        }
+    }
+    concatenate[lenConc] = '\0';
+    return concatenate;
 }
 
 int mystrfind(char *origin, char *substr){
+    int lenOr = mystrlen(origin);
+    int lenSu = mystrlen(substr);
+    if (lenSu > lenOr) {
+        return 0;
+    }
+    for(int i=0;i<(lenOr);i++){
+        if (origin[i]==substr[0]){
+            if(lenSu==1){
+                return 1;
+            }
+            for(int j=1;j<lenSu;j++){
+                if(origin[i+j]!=substr[j]){
+                    break;
+                }
+            }
+            return 1;
+        }
+    }
     return 0;
 }
