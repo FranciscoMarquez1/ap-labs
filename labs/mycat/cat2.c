197d01bdcd28a17cefbdceddcd80922315556594
FranciscoMarquez1 <fcomarquezb@hotmail.com>
2020-03-23 22:29:20 -0600

    Solved mycat lab

--- labs/mycat/cat2.c
+++ labs/mycat/cat2.c
@@ -1,33 +1,32 @@
+ 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-/* filecopy:  copy file ifp to file ofp */
-void filecopy(FILE *ifp, FILE *ofp)
-{
-    int c;
-
-    while ((c = getc(ifp)) != EOF)
-        putc(c, ofp);
-
-}
+void filecopy(int fd);
 
 /* cat:  concatenate files, version 2 */
 int main(int argc, char *argv[])
 {
-    FILE *fp;
-    void filecopy(FILE *, FILE *);
+    int fd;
+    void filecopy(int fd);
     char *prog = argv[0];   /* program name for errors */
 
     if (argc == 1)  /* no args; copy standard input */
-        filecopy(stdin, stdout);
+        filecopy(0);
     else
         while (--argc > 0)
-            if ((fp = fopen(*++argv, "r")) == NULL) {
+            if ((fd = open(*++argv, O_RDONLY)) == -1) {
                 fprintf(stderr, "%s: can′t open %s\n",
 			prog, *argv);
                 return 1;
             } else {
-                filecopy(fp, stdout);
-                fclose(fp);
+                filecopy(fd);
+                close(fd);
             }
 
     if (ferror(stdout)) {
@@ -36,4 +35,25 @@ int main(int argc, char *argv[])
     }
 
     return 0;
+}
+
+void filecopy(int fd)
+{
+
+    if(fd == 0){
+        int size = 512;
+        char buffer[size+1];
+        while(read(fd,buffer,size)>0){
+            buffer[size] = '\0';
+            printf("%s", buffer);
+            memset(buffer,0,size);
+        }
+        return;
+    }
+
+    off_t fs = lseek(fd,(size_t)0,SEEK_END);
+    lseek(fd,(size_t)0,SEEK_SET);
+    char buff[fs+1];
+    read(fd, buff, fs);
+    write(1, buff, fs);
 }
